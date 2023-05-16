c3ccc3ec78dca1d6eeb772592cf31646696c9e02
Srikanth <srikanth@vestedfinance.co>
2023-05-16 21:54:44 +0530

    Added mongoose client and connection for mongodb.

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 4545;
 
-const { USERNAME, PASSWORD, DATABASE, DATABASE_URL } = process.env;
+const { DATABASE, DATABASE_URL } = process.env;
 
-if (!USERNAME || !PASSWORD || !DATABASE || !DATABASE_URL) {
+if (!DATABASE || !DATABASE_URL) {
   throw new Error(
     "Env vars not found. `cp .env.sample .env` and fill env values in .env"
   );
 }
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
